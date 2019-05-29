e554fd3116d29d6bf3876f5d0b8ebb5a85fdb409
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-05-29 17:16:00 -0400

    added items class and added items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,32 @@
 from room import Room
 
+from item import Item
+
+items = {
+    "sword": Item("guilded sword... ", "A perfect item for slaying your enimies."),
+    "shield": Item("diamond shield... ", " Wonder why some one is leaving such expensive equipment behind... kind of strange..."),
+    "pendant": Item("a shiny pendant... ", "the pendant contains a strange message in a language that you cant comprehend."),
+}
+
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [items["sword"], items["shield"]]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [items["pendant"],]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
 }
 
 
@@ -77,8 +85,16 @@ def move_player(curRoom, direction):
 
 
 while True:
-    print(f"\n{new_player.name} finds him/herself {new_player.current_room.name}")
-    print(f"{new_player.current_room.description}")
+    
+    print(f"\n{new_player.name} finds him/herself {new_player.current_room.name}... \n")
+    print(f"{new_player.current_room.description}... \n")
+    
+    if len(new_player.current_room.items) > 0:
+        print("    you see some items in this room\n")
+        for i in new_player.current_room.items:
+            print(f"        {i.name} {i.description}")
+    else:
+        print(f"    There are no items to be seen in this room")
 
     direction = input("\nwhich direction do you wish to travel from here? (n, s, e, w):")
     if direction == "q":
