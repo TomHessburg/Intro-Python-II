2c380441c1cbf7c877e1cdb0e636f032010af590
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-05-30 14:16:54 -0400

    fixed bug with taking/dropping items that dont exist. cleaned up code a bit

--- src/adv.py
+++ src/adv.py
@@ -1,12 +1,13 @@
+from player import Player
 from room import Room
 
 from item import Item
 
 items = {
-    "map": Item("map... ", "this should help me in my journy."),
-    "sword": Item("sword... ", "A perfect item for slaying your enimies."),
-    "shield": Item("shield... ", " Defend from your foes..."),
-    "shiny-pendant": Item("shiny-pendant... ", "the pendant contains a strange message in a language that you cant comprehend."),
+    "map": Item("map", "...this should help me in my journy."),
+    "sword": Item("sword", "...a perfect item for slaying your enimies."),
+    "shield": Item("shield", "...defend from your foes..."),
+    "shiny-pendant": Item("shiny-pendant", "...the pendant contains a strange message in a language that you cant comprehend."),
 }
 
 # Declare all the rooms
@@ -16,7 +17,7 @@ room = {
                      "North of you, the cave mount beckons", [items["sword"], items["shield"]]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", [items["shiny-pendant"],]),
+passages run north and east.""", [items["shiny-pendant"], ]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -43,98 +44,97 @@ room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
 
-#make player
-from player import Player
+# make player
 
 player_name = input("Please enter your name, adventurer!: ")
 
 new_player = Player(player_name, room["outside"], [items["map"]])
-print("(n, s, e, w) to move, 'take' or 'drop' to interact with items, 'i' or 'inventory' to see what youre holding...")
-
-
+print("(n, s, e, w) to move, 'take' or 'drop' to interact with items, 'i' or 'inventory' to see what youre holding...\n\n\n")
 
 
 def move_player(curRoom, direction):
     if direction == "n":
         if curRoom.n_to != None:
             new_player.current_room = curRoom.n_to
-            
+            print(f"{new_player.current_room.name}... ")
+            print(f"{new_player.current_room.description}... ")
         else:
-            print("\nYou can not go that way, adventuruer!")
+            print("You can not go that way, adventuruer!")
     elif direction == "s":
         if curRoom.s_to != None:
             new_player.current_room = curRoom.s_to
-            
+            print(f"{new_player.current_room.name}... ")
+            print(f"{new_player.current_room.description}... ")
         else:
-            print("\nYou can not go that way, adventuruer!")
+            print("You can not go that way, adventuruer!")
 
     elif direction == "e":
         if curRoom.e_to != None:
             new_player.current_room = curRoom.e_to
-            
+            print(f"{new_player.current_room.name}... ")
+            print(f"{new_player.current_room.description}... ")
         else:
-            print("\nYou can not go that way, adventuruer!")
+            print("You can not go that way, adventuruer!")
 
     else:
         if curRoom.w_to != None:
             new_player.current_room = curRoom.w_to
-            
+            print(f"{new_player.current_room.name}... ")
+            print(f"{new_player.current_room.description}... ")
         else:
-            print("\nYou can not go that way, adventuruer!")
-    
+            print("You can not go that way, adventuruer!")
+
+print(f"{new_player.current_room.name}... ")
+print(f"{new_player.current_room.description}... ")
 
 
 while True:
-    
-    print(f"\n{new_player.current_room.name}... ")
-    print(f"{new_player.current_room.description}... \n")
-    
-    ##print items in room
+    # print items in room
     if len(new_player.current_room.items) > 0:
-        print("you see some items in this room\n")
+        print("you see some items in this room")
         for i in new_player.current_room.items:
             print(f"    {i.name} {i.description}")
     else:
         print(f"There are no items to be seen in this room")
 
-    #check for win state
+    # check for win state
     if new_player.current_room.name == "Treasure Chamber" and items["shiny-pendant"] in new_player.current_room.items:
-        print("\n\n\nThe gods are pleased that you have returned the treasre.. Thank you for restoring the peace.")
+        print("The gods are pleased that you have returned the treasre.. Thank you for restoring the peace.")
         break
 
-    #action input handlers
-    action = input("\n\n\n\n\nWhat would you wish to do?:")
-    #quit
+    # action input handlers
+    action = input("\n\nWhat would you wish to do?:")
+    # quit
     if action == "q":
         break
-    #move
-    elif action == "n" or action == "s" or action == "e" or action == "w":
+    # move
+    elif action in ["n", "s", "e", "w"]:
         move_player(new_player.current_room, action)
-    #take an item
+    # take an item
     elif "take" in action:
         item = action[5:]
-        if items[item] in new_player.current_room.items:
+        if item in [item.name for item in new_player.current_room.items]:
             new_player.on_take(item)
             new_player.items.append(items[item])
             new_player.current_room.items.remove(items[item])
         else:
             print("that items dosnt exist here..")
-    #drop an item
+    # drop an item
     elif "drop" in action:
         item = action[5:]
-        if items[item] in new_player.items:  
+        if item in [item.name for item in new_player.items]:
             new_player.items.remove(items[item])
             new_player.current_room.items.append(items[item])
         else:
             print("You dont have that item to drop!")
-    #show inventory
+    # show inventory
     elif action == "i":
         new_player.tell_items()
     elif action == "inventory":
         new_player.tell_items()
-    #dont allow other input
+    # dont allow other input
     else:
-        print("\nSorry, but i cant read that input!")
+        print("Sorry, but i cant read that input!")
 
 
 print("See you soon, adventurer!")
--- src/player.py
+++ src/player.py
@@ -1,17 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
 class Player:
     def __init__(self, name, current_room, items):
         self.name = name
         self.current_room = current_room
         self.items = items
+
     def tell_items(self):
         if len(self.items) > 0:
-            print("\nyoure currently carrying:\n")
+            print("\nyoure currently carrying:")
             for i in self.items:
                 print(f"    {i.name} {i.description}")
         else:
             print("\nyou arent currently carrying any items")
-    
+
     def on_take(self, item_name):
         print(f"\n{self.name} picked up {item_name}")
